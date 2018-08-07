3f775c1d4d38dd8ade0b12666af071ed1cfbc876
Suzanna Krivulskaya <skrivulskaya@users.noreply.github.com>
2018-08-07 18:59:10 -0400

    updated app design to dashboard

--- BadinApp/app.R
+++ BadinApp/app.R
@@ -1,26 +1,23 @@
-#
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
-#
 # Find out more about building applications with Shiny here:
-#
 #    http://shiny.rstudio.com/
-#
 
-rm(list=ls(all=TRUE)) #clear memory
+#clear memory
+rm(list=ls(all=TRUE))
 
+#load required packages
 library(shiny)
 library(rgdal)
 library(leaflet)
 library(rsconnect)
+library(shinydashboard)
 
-
-#Data Prep
+#load data files
 md.geocoded <- read.csv("rural_md_geocoded.csv", stringsAsFactors = F)
 md.slave.data <- read.csv("enslaved_people_list.csv", stringsAsFactors = F)
 
-
-#randomizingPoints
+#randomize points on the map
 md.geocoded$latlong <- paste(md.geocoded$lat,md.geocoded$lon,sep="-")
 a<- data.frame(table(md.geocoded$latlong))
 a$latlong <- as.character(a$Var1)
@@ -30,20 +27,16 @@ remove(a)
 md.geocoded$lat_edit <-ifelse((md.geocoded$Freq > 1),  md.geocoded$lat - (runif(nrow(md.geocoded))-.5)/40,md.geocoded$lat)
 md.geocoded$lon_edit <-ifelse((md.geocoded$Freq > 1), md.geocoded$lon - (runif(nrow(md.geocoded))-.5)/40,md.geocoded$lon)
 md.geocoded$latlong<- NULL
-
 md.geocoded$SlavOwnerText <- "Unknown"
 md.geocoded[!is.na(md.geocoded$SlaveOwner),]$SlavOwnerText <- "Confirmed"
 
-
-#end RandomizingPoints
-
-
-
-
+#map and color legend
 md.spdf <-  SpatialPointsDataFrame(coords = md.geocoded[,c("lon_edit","lat_edit")], data = md.geocoded,
                                    proj4string = CRS("+proj=longlat +datum=WGS84"))
 pal <- colorFactor(palette = 'Set1', domain =md.geocoded$LocationConfidenceLevel)
 pal.slaves <- colorFactor(palette = 'Set1', domain =md.geocoded$SlavOwnerText)
+
+#generate html popup
 md.spdf$popupw <- paste(sep = "",  "<b>", md.spdf$ShinyName,"</b><br/>",
                         "Years: ", ifelse(is.na(md.spdf$ShinyDates), "Unknown", md.spdf$ShinyDates),"<br/>",
                          # "Number Copies: ",md.spdf$SubscriberNoCopies, "<br/>",
@@ -55,37 +48,34 @@ md.spdf$popupw <- paste(sep = "",  "<b>", md.spdf$ShinyName,"</b><br/>",
                         "Notes: ",ifelse(is.na(md.spdf$ShinyNote),"N/A",md.spdf$ShinyNote),"<br/>"
                     ) #end html popup
 
-
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Badin Bible Maryland Viewer"),
-   
-   # MainPanel row
-   fluidRow(
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-     sidebarPanel(
-        checkboxInput("slaves", "Confirmed Slave Owners Only", value = FALSE, width = NULL),
-        checkboxInput("priests", "Priests Only", value = FALSE, width = NULL)
-      ),
-      mainPanel(leafletOutput("mymap"))
-   )),#end MainPanel
-   hr()
-   
-   # Output tables row <-develop further
-   ,
-   fluidRow(
-     column(2, h5("Summary Chart: "))
-   ),
-   fluidRow(
-     column(12,plotOutput("chartTest"))
-   )
+#build Shiny interface
+ui <- dashboardPage(
+  dashboardHeader(title = "Badin Bible Subscribers"),
+  dashboardSidebar(
+    checkboxInput("slaves", "Confirmed Slave Owners Only", value = FALSE, width = NULL),
+    checkboxInput("priests", "Priests Only", value = FALSE, width = NULL),
+  sidebarMenu(
+    menuItem("Map", tabName = "map"),
+    menuItem("Raw Data", tabName = "rawdata"),
+    menuItem("Network", tabName = "network")
+  )),
+  dashboardBody(
+    tabItems(
+      tabItem("map",
+              fluidRow(
+                box(width = 9, status = "primary", solidHeader = TRUE,
+                  title = "Subscribers in Rural Maryland",
+                  leafletOutput("mymap", height = 500)),
+                box(width = 3, status = "primary", solidHeader = TRUE, collapsible = TRUE,
+                  title = "Household Size", 
+                  plotOutput("chartTest", height = 200)),
+                box(width = 3, status = "primary", solidHeader = TRUE, collapsible = TRUE,
+                    title = "Another Output Example", 
+                    plotOutput("anotherOutput", height = 200))
+                ))))
 )
 
-# Define server logic required to draw a histogram
+#define server logic
 server <- function(input, output) {
     points <- eventReactive(c(input$slaves, input$priests), {
       working.spdf <- md.spdf
@@ -99,7 +89,7 @@ server <- function(input, output) {
       return(working.spdf)
     }, ignoreNULL = FALSE)
     
-# Reactive map output    
+#reactive map output    
    output$mymap <- renderLeaflet({
      leaflet() %>%
        addProviderTiles(providers$Stamen.TonerLite,
@@ -108,16 +98,16 @@ server <- function(input, output) {
        addCircleMarkers(data = points(),color = ~pal(LocationConfidenceLevel), popup = ~popupw) %>%
        addLegend("bottomleft",pal = pal,values=points()$LocationConfidenceLevel, opacity = 1)
    })
+   
+#define server logic required to draw a histogram
    output$chartTest<-renderPlot(
      hist(points()@data$CensusNumberOfHouseholdMembers)
    )
-
 }
-# pal <- colorFactor(palette = 'Set1', domain =md.geocoded$LocationConfidenceLevel)
 
-# Run the application 
+#run the application 
 shinyApp(ui = ui, server = server)
 
-# Update the application in Shinyapps.io
+#update the application in Shinyapps.io
 # library(rsconnect)
 # deployApp()
