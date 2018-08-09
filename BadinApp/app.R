b512024b2ab96f3dcc6a06f02c523f07cc8b2fd4
Suzanna Krivulskaya <skrivulskaya@users.noreply.github.com>
2018-08-08 23:06:45 -0400

    added network visualization tab

--- BadinApp/app.R
+++ BadinApp/app.R
@@ -12,11 +12,24 @@ library(rgdal)
 library(leaflet)
 library(rsconnect)
 library(shinydashboard)
+library(visNetwork)
 
 #load data files
 md.geocoded <- read.csv("rural_md_geocoded.csv", stringsAsFactors = F)
 md.slave.data <- read.csv("enslaved_people_list.csv", stringsAsFactors = F)
+clergy.net <- read.csv("priest_network.csv", header = T, as.is = T)
 
+#CLERGY NETWORK SECTION
+#generate html popup to use as node label for network visualization
+clergy.net$popupw <- paste(sep = "", "<b>",clergy.net$ShinyName,"</b><br/>",
+                           "Years: ", ifelse(is.na(clergy.net$ShinyDates), "Unknown", clergy.net$ShinyDates),"<br/>",
+                           "Bible copies: ", ifelse(is.na(clergy.net$SubscriberNoCopies), "Unknown", clergy.net$SubscriberNoCopies),"<br/>",
+                           "Enslaved People: ",ifelse(is.na(clergy.net$NumberSlaves),"Unknown",clergy.net$NumberSlaves), "<br/>",
+                           "Notes: ",ifelse(is.na(clergy.net$NetworkNote),"N/A",clergy.net$NetworkNote),"<br/>"
+) #end html popup
+
+
+#MAPPING SECTION
 #randomize points on the map
 md.geocoded$latlong <- paste(md.geocoded$lat,md.geocoded$lon,sep="-")
 a<- data.frame(table(md.geocoded$latlong))
@@ -48,6 +61,7 @@ md.spdf$popupw <- paste(sep = "",  "<b>", md.spdf$ShinyName,"</b><br/>",
                         "Notes: ",ifelse(is.na(md.spdf$ShinyNote),"N/A",md.spdf$ShinyNote),"<br/>"
                     ) #end html popup
 
+#SHINY SECTION
 #build Shiny interface
 ui <- dashboardPage(
   dashboardHeader(title = "Badin Bible Subscribers"),
@@ -56,8 +70,8 @@ ui <- dashboardPage(
     checkboxInput("priests", "Priests Only", value = FALSE, width = NULL),
   sidebarMenu(
     menuItem("Map", tabName = "map"),
-    menuItem("Raw Data", tabName = "rawdata"),
-    menuItem("Network", tabName = "network")
+    menuItem("Network", tabName = "network"),
+    menuItem("Raw Data", tabName = "rawdata")
   )),
   dashboardBody(
     tabItems(
@@ -72,8 +86,13 @@ ui <- dashboardPage(
                 box(width = 3, status = "primary", solidHeader = TRUE, collapsible = TRUE,
                     title = "Another Output Example", 
                     plotOutput("anotherOutput", height = 200))
-                ))))
-)
+                )),
+      tabItem("network",
+              fluidRow(visNetworkOutput("network", height = 600)
+              )),
+      tabItem("rawdata")
+    )
+))
 
 #define server logic
 server <- function(input, output) {
@@ -103,6 +122,37 @@ server <- function(input, output) {
    output$chartTest<-renderPlot(
      hist(points()@data$CensusNumberOfHouseholdMembers)
    )
+   
+#visNetwork output
+   output$network <- renderVisNetwork({
+     #create nodes
+     nodes <- data.frame(id = 1:29,
+                         label = paste(clergy.net$ShinyName), #node label
+                         title = paste(clergy.net$popupw), #text on click
+                         group = paste(clergy.net$Group) #group by status to assign different shapes to nodes
+     )
+     #create connections
+     edges <- data.frame(from = c(1,1,1,1,1,1,1,1,1,1,
+                                  1,1,1,1,1,1,1,1,1,1,
+                                  1,1,1,1,1,1,1,1,
+                                  10,11,28,16,7,7,8,
+                                  12,13,13,27,27,
+                                  19,19,26,26,26,26,26,
+                                  20,20,20,20,21,21,
+                                  29,29,18), 
+                         to = c(2:29,
+                                28,28,2,29,8,9,9,
+                                2,19,20,18,2,
+                                20,21,20,21,29,18,2,
+                                21,29,18,2,29,2,
+                                18,2,2))
+     #create visNetwork
+     visNetwork(nodes, edges) %>%
+       #define shapes and colors for the clergy
+       visGroups(groupname = "Clergy", color = list(background = "lightgray",border="darkgray"), shape = "box", borderWidth = 1) %>% 
+       # define shapes and colors for the lay subscribers
+       visGroups(groupname = "Lay", shape = "box")
+})
 }
 
 #run the application 
